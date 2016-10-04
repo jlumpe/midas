44fce0b94e9b781c93b960128ce5dec31397af5a
Jared Lumpe <mjlumpe@gmail.com>
2016-10-03 22:45:40 -0700

    Move version definition to separate file, clean up setup.py

--- midas/__init__.py
+++ midas/__init__.py
@@ -4,3 +4,5 @@ Microbial IDentification And Surveillance through whole genome sequencing data
 Author: Jared Lumpe
 Author email: mjlumpe@gmail.com
 """
+
+from .version import __version__
--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 """setuptools installation script for midas package"""
 
 from setuptools import setup, find_packages
+from distutils.util import convert_path
 from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
 
 
+# Get package version without importing it
+version_ns = dict()
+with open(convert_path('midas/version.py')) as fobj:
+	exec(fobj.read(), version_ns)
+version = version_ns['__version__']
+
+
+# Dependencies
+install_requires = [
+	'numpy~=1.11',
+	'sqlalchemy~=1.0',
+	'cython~=0.24',
+]
+
+
+# Cython extensions
 np_include = numpy.get_include()
-
-
 extensions = [
 	Extension('midas.cython.*', ['midas/cython/*.pyx'],
 	          include_dirs=[np_include]),
@@ -17,16 +32,14 @@ extensions = [
 
 setup(
 	name='midas',
-	version='1.0.0',
-	description=('Microbial IDentification And Surveillance through whole '
-	             'genome sequencing data'),
+	version=version,
+	description=(
+		'Microbial IDentification And Surveillance through whole genome '
+		'sequencing data'
+	),
 	author='Jared Lumpe',
 	author_email='mjlumpe@gmail.com',
 	packages=find_packages(),
-	install_requires=[
-		'numpy~=1.11',
-		'sqlalchemy~=1.0',
-		'cython~=0.24',
-	],
+	install_requires=install_requires,
 	ext_modules=cythonize(extensions),
 )
