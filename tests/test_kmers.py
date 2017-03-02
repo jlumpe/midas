92e7d62b58d5a5ac98656c4234488bffea50c8e6
Jared Lumpe <mjlumpe@gmail.com>
2017-03-01 16:51:30 -0800

    Rearrange midas.kmers tests, add tests for KmerCoordsCollection

--- tests/test_kmers.py
+++ tests/test_kmers.py
@@ -101,6 +101,9 @@ def test_vec_coords_conversion():
 		for index in coords:
 			assert vec[index]
 
+		# Check coords ascending
+		assert np.all(np.diff(coords) > 0)
+
 		# Check converting back
 		assert np.array_equal(vec, spec.coords_to_vec(coords))
 
@@ -248,142 +251,248 @@ def make_kmer_seq(seqlen, k, prefix_len, kmer_interval, n_interval=None, seed=0)
 	return bytes(seq_array), kspec, vec
 
 
-def test_find_kmers():
-	"""Test general k-mer finding."""
-
-	seq, kspec, vec = make_kmer_seq(100000, k=11, prefix_len=5, kmer_interval=50,
-	                                n_interval=10)
-
-	# Test normal
-	assert np.array_equal(kmers.find_kmers(kspec, seq), vec)
-
-	# Test reverse complement
-	assert np.array_equal(kmers.find_kmers(kspec, reverse_complement(seq)), vec)
-
-	# Test lower case
-	assert np.array_equal(kmers.find_kmers(kspec, seq.lower()), vec)
-
-	# Test string argument
-	assert np.array_equal(kmers.find_kmers(kspec, seq.decode('ascii')), vec)
+class TestFindKmers:
+	"""Test k-mer finding."""
 
 
-def test_find_kmers_bounds():
-	"""
-	Test k-mer finding at beginning and end of sequence to catch errors with
-	search bounds.
-	"""
+	def test_basic(self):
+		"""Test general k-mer finding."""
 
-	# Sequence of all ATN's
-	seqlen = 100000
-	seq_array = fill_bytearray(b'ATN', seqlen)
-
-	# Choose prefix with nucleotides not found in sequence "background"
-	kspec = kmers.KmerSpec(11, b'CCGGG')
-
-	# Add at beginning
-	seq_array[0:kspec.prefix_len] = kspec.prefix
-	seq_array[kspec.prefix_len:kspec.total_len] = kmers.index_to_kmer(0, kspec.k)
-
-	# Add at end
-	seq_array[-kspec.total_len:-kspec.k] = kspec.prefix
-	seq_array[-kspec.k:] = kmers.index_to_kmer(1, kspec.k)
-
-	seq = bytes(seq_array)
-	found_coords = kmers.vec_to_coords(kmers.find_kmers(kspec, seq))
-
-	assert np.array_equal(found_coords, [0, 1])
-
-
-def test_find_kmers_overlapping():
-	"""Test k-mer finding when k-mers overlap with each other.
-
-	The test sequence is manually designed to have a variety of overlapping
-	forwards and backwards matches
-	"""
-
-	kspec = kmers.KmerSpec(11, b'GCCGG')
-
-	seq = b'ATATGCCGGCCGGATTATATAGCCGGCATTACATCCGATAGGATCCGGCAATAA'
-	#      |    |>>>>...........
-	#      |        |>>>>........... (forward match which overlaps prefix)
-	#      |                     |>>>>........... (another overlapping forward match)
-	#      |....<<<<| (backward match for prefix, but too close to end)
-	#      |           ...........<<<<|
-	#      |                                 ...........<<<<|
-
-	expected = {
-		b'CCGGATTATAT',
-		b'ATTATATAGCC',
-		b'CATTACATCCG',
-		reverse_complement(b'GGATTATATAG'),
-		reverse_complement(b'TCCGATAGGAT'),
-	}
-
-	for s in [seq, reverse_complement(seq)]:
-		vec = kmers.find_kmers(kspec, s)
-
-		found = [
-			kmers.index_to_kmer(i, kspec.k)
-			for i in kmers.vec_to_coords(vec)
-		]
-
-		assert len(found) == len(expected)
-		assert all(kmer in expected for kmer in found)
-
-
-def test_find_kmers_parse():
-	"""Test k-mer finding in FASTA files."""
-
-	from io import StringIO
-
-	from Bio.Seq import Seq
-	from Bio.SeqIO import write as write_seqs, SeqRecord
-
-	k = 11
-
-	vec = np.zeros(4 ** k, dtype=bool)
-	kspec = None
-
-	records = []
-
-	# Create FASTA file with 10 records
-	for i in range(10):
-
-		seq, _kspec, rec_vec = make_kmer_seq(
-			100000,
-			k=k,
+		seq, kspec, vec = make_kmer_seq(
+			100000, k=11,
 			prefix_len=5,
 			kmer_interval=50,
-			n_interval=10,
-			seed=i,
+			n_interval=10
 		)
 
-		# Combine vectors of all sequences
-		vec |= rec_vec
+		# Test normal
+		assert np.array_equal(kmers.find_kmers(kspec, seq), vec)
 
-		# Just check that our specs are all the same...
-		if kspec is None:
-			kspec = _kspec
-		else:
-			assert kspec == _kspec
+		# Test reverse complement
+		assert np.array_equal(kmers.find_kmers(kspec, reverse_complement(seq)), vec)
 
-		# Convert every other sequence to lower case, just to switch things up...
-		if i % 2:
-			seq = seq.lower()
+		# Test lower case
+		assert np.array_equal(kmers.find_kmers(kspec, seq.lower()), vec)
 
-		# Create the BioPython sequence record object
-		records.append(SeqRecord(
-			seq=Seq(seq.decode('ascii')),
-			id='SEQ{}'.format(i + 1),
-			description='sequence {}'.format(i + 1),
-		))
+		# Test string argument
+		assert np.array_equal(kmers.find_kmers(kspec, seq.decode('ascii')), vec)
 
-	# Write records to string buffer in FASTA format
-	buf = StringIO()
-	write_seqs(records, buf, 'fasta')
-	buf.seek(0)
 
-	# Parse from buffer
-	parsed_vec = kmers.find_kmers_parse(kspec, buf, 'fasta')
+	def test_bounds(self):
+		"""
+		Test k-mer finding at beginning and end of sequence to catch errors with
+		search bounds.
+		"""
 
-	assert np.array_equal(vec, parsed_vec)
+		# Sequence of all ATN's
+		seqlen = 100000
+		seq_array = fill_bytearray(b'ATN', seqlen)
+
+		# Choose prefix with nucleotides not found in sequence "background"
+		kspec = kmers.KmerSpec(11, b'CCGGG')
+
+		# Add at beginning
+		seq_array[0:kspec.prefix_len] = kspec.prefix
+		seq_array[kspec.prefix_len:kspec.total_len] = kmers.index_to_kmer(0, kspec.k)
+
+		# Add at end
+		seq_array[-kspec.total_len:-kspec.k] = kspec.prefix
+		seq_array[-kspec.k:] = kmers.index_to_kmer(1, kspec.k)
+
+		seq = bytes(seq_array)
+		found_coords = kmers.vec_to_coords(kmers.find_kmers(kspec, seq))
+
+		assert np.array_equal(found_coords, [0, 1])
+
+
+	def test_overlapping(self):
+		"""Test k-mer finding when k-mers overlap with each other.
+
+		The test sequence is manually designed to have a variety of overlapping
+		forwards and backwards matches
+		"""
+
+		kspec = kmers.KmerSpec(11, b'GCCGG')
+
+		seq = b'ATATGCCGGCCGGATTATATAGCCGGCATTACATCCGATAGGATCCGGCAATAA'
+		#      |    |>>>>...........
+		#      |        |>>>>........... (forward match which overlaps prefix)
+		#      |                     |>>>>........... (another overlapping forward match)
+		#      |....<<<<| (backward match for prefix, but too close to end)
+		#      |           ...........<<<<|
+		#      |                                 ...........<<<<|
+
+		expected = {
+			b'CCGGATTATAT',
+			b'ATTATATAGCC',
+			b'CATTACATCCG',
+			reverse_complement(b'GGATTATATAG'),
+			reverse_complement(b'TCCGATAGGAT'),
+		}
+
+		for s in [seq, reverse_complement(seq)]:
+			vec = kmers.find_kmers(kspec, s)
+
+			found = [
+				kmers.index_to_kmer(i, kspec.k)
+				for i in kmers.vec_to_coords(vec)
+			]
+
+			assert len(found) == len(expected)
+			assert all(kmer in expected for kmer in found)
+
+
+	def test_parse(self):
+		"""Test k-mer finding in FASTA files."""
+
+		from io import StringIO
+
+		from Bio.Seq import Seq
+		from Bio.SeqIO import write as write_seqs, SeqRecord
+
+		k = 11
+
+		vec = np.zeros(4 ** k, dtype=bool)
+		kspec = None
+
+		records = []
+
+		# Create FASTA file with 10 records
+		for i in range(10):
+
+			seq, _kspec, rec_vec = make_kmer_seq(
+				100000,
+				k=k,
+				prefix_len=5,
+				kmer_interval=50,
+				n_interval=10,
+				seed=i,
+			)
+
+			# Combine vectors of all sequences
+			vec |= rec_vec
+
+			# Just check that our specs are all the same...
+			if kspec is None:
+				kspec = _kspec
+			else:
+				assert kspec == _kspec
+
+			# Convert every other sequence to lower case, just to switch things up...
+			if i % 2:
+				seq = seq.lower()
+
+			# Create the BioPython sequence record object
+			records.append(SeqRecord(
+				seq=Seq(seq.decode('ascii')),
+				id='SEQ{}'.format(i + 1),
+				description='sequence {}'.format(i + 1),
+			))
+
+		# Write records to string buffer in FASTA format
+		buf = StringIO()
+		write_seqs(records, buf, 'fasta')
+		buf.seek(0)
+
+		# Parse from buffer
+		parsed_vec = kmers.find_kmers_parse(kspec, buf, 'fasta')
+
+		assert np.array_equal(vec, parsed_vec)
+
+
+class TestKmerCoordsCollection:
+	"""Test KmerCoordsCollection."""
+
+	@pytest.fixture()
+	def coords_list(self):
+		"""List of k-mer coordinate arrays."""
+
+		random = np.random.RandomState(seed=0)
+
+		ncoords = 100
+		nkmers_range = (5000, 15000)
+
+		cl = [
+			np.arange(random.randint(*nkmers_range)) * ncoords + i
+			for i in range(ncoords)
+		]
+
+		# Add a zero-length set in there just to throw everything off
+		cl.append(np.empty(0, dtype=cl[0].dtype))
+
+		return cl
+
+	@pytest.fixture()
+	def kcol(self, coords_list):
+		"""Create from coords_list using from_coords_seq()."""
+		return kmers.KmerCoordsCollection.from_coords_seq(coords_list)
+
+	def test_basic(self, kcol, coords_list):
+		"""Test item access and calling methods without side effects."""
+
+		# Check correct length
+		assert len(kcol) == len(coords_list)
+
+		# Check each coordinate set matches
+		for i, coords in enumerate(coords_list):
+
+			assert np.array_equal(kcol[i], coords)
+			assert kcol.size_of(i) == len(coords)
+
+	def test_invalid_index(self, kcol):
+		"""Test passing invalid indices."""
+
+		# Check negative integer index
+		with pytest.raises(IndexError):
+			kcol[-1]
+		with pytest.raises(IndexError):
+			kcol[-1] = 0
+
+		# Test integer index too large
+		with pytest.raises(IndexError):
+			kcol[len(kcol)]
+		with pytest.raises(IndexError):
+			kcol[len(kcol)] = 0
+
+		# Test slice
+		with pytest.raises(TypeError):
+			kcol[:]
+		with pytest.raises(TypeError):
+			kcol[:] = 0
+
+	def test_from_empty(self, coords_list):
+		"""Test creating from empty array."""
+
+		lengths = list(map(len, coords_list))
+
+		kcol = kmers.KmerCoordsCollection.empty(lengths)
+
+		assert len(kcol) == len(coords_list)
+
+		for i in range(len(kcol)):
+			assert kcol.size_of(i) == len(coords_list[i])
+
+	def test_assignment(self, kcol):
+		"""Test item assignment."""
+
+		for i in range(len(kcol)):
+
+			# Assign to single number
+			n = i * 11 % 23
+			kcol[i] = n
+			assert np.all(kcol[i] == n)
+
+			# Assign to array
+			a = (np.arange(kcol.size_of(i)) * 11 + i) % 23
+			kcol[i] = a
+			assert np.array_equal(kcol[i], a)
+
+	def test_empty(self):
+		"""Really an edge case, but test it anyways."""
+
+		kcol = kmers.KmerCoordsCollection.from_coords_seq([])
+
+		assert len(kcol) == 0
+
+		with pytest.raises(IndexError):
+			kcol[0]
