f7866563e75436f14c043569a500bcdf7d21e62c
Jared Lumpe <mjlumpe@gmail.com>
2017-08-01 20:44:03 -0700

    Split up SignatureFile tests

--- tests/io/test_signaturefile.py
+++ tests/io/test_signaturefile.py
@@ -10,68 +10,96 @@ from midas.kmers import SignatureArray
 
 
 @pytest.fixture(scope='module')
-def signatures():
-	"""List of random signatures."""
+def sigarray():
+	"""Array of random signatures."""
 
 	random = np.random.RandomState(0)
 
-	lengths = random.randint(2000, 10000, size=10)
+	lengths = random.randint(2000, 10000, size=20)
 	choices = np.arange(4 ** 11)
 
-	return [
+	return SignatureArray.from_signatures([
 		np.sort(random.choice(choices, replace=False, size=len_))
 		for len_ in lengths
-	]
+	])
 
 
 @pytest.fixture(params=[None, 'strings', 'ints'])
-def ids(signatures, request):
+def ids(sigarray, request):
+	"""None or list of strings or ints."""
 	if request.param == 'strings':
-		return ['id' + str(i) for i in range(len(signatures))]
+		return ['id' + str(i) for i in range(len(sigarray))]
 
 	elif request.param == 'ints':
-		return list(range(len(signatures)))
+		return list(range(len(sigarray)))
 
 
 @pytest.fixture(params=[False, True])
 def metadata(request):
+	"""None or a dict of fake metadata."""
 	if request.param:
 		return dict(foo=1, bar='z', baz=[5, 4, 1], bleh=dict(a=1, b=None))
 	else:
 		return None
 
 
-def test_readwrite(signatures, ids, metadata):
-
-	sigarray = SignatureArray.from_signatures(signatures)
+@pytest.fixture
+def signature_buffer(sigarray, ids, metadata):
+	"""Binary buffer with signature file written to it."""
 
 	buf = io.BytesIO()
-
-	# Write
 	SignatureFile.write(buf, sigarray, ids=ids, metadata=metadata)
 	buf.seek(0)
 
-	# Read
-	sigfile = SignatureFile(buf)
+	return buf
 
-	assert sigfile.count == len(signatures)
-	assert np.array_equal(sigfile.lengths, list(map(len, signatures)))
 
-	# Test reading whole array
-	sigarray2 = sigfile.get_array()
-	assert np.array_equal(sigarray.values, sigarray2.values)
-	assert np.array_equal(sigarray.bounds, sigarray2.bounds)
+@pytest.fixture
+def sigfile(signature_buffer):
+	"""SignatureFile read from buffer."""
+	return SignatureFile(signature_buffer)
 
-	# Test iterating over signatures
-	for sig1, sig2 in zip(sigarray, sigfile.iter_signatures()):
-		assert np.array_equal(sig1, sig2)
 
-	# Check IDs match
+def test_attrs(sigarray, sigfile):
+	"""Test basic attributes set when file is first opened."""
+
+	assert sigfile.count == len(sigarray)
+	assert np.array_equal(sigfile.lengths, list(map(len, sigarray)))
+	assert sigfile.nelems == sum(map(len, sigarray))
+	assert sigfile.dtype == sigarray.values.dtype
+
+
+def test_ids(sigarray, sigfile, ids):
+	"""Test the ids attribute matches what was written."""
+
 	if ids is None:
 		assert sigfile.ids is None
 
 	else:
 		assert np.array_equal(ids, sigfile.ids)
 
-	# Check metadata
-	assert sigfile.get_metadata() == metadata
+
+def test_metadata(sigarray, sigfile, metadata):
+	"""Test the file metadata matches what was written."""
+
+	if metadata is None:
+		assert not sigfile.has_metadata
+		assert sigfile.get_metadata() is None
+	else:
+		assert sigfile.has_metadata
+		assert sigfile.get_metadata() == metadata
+
+
+def test_read_all(sigarray, sigfile):
+	"""Test reading all signatures into an array."""
+
+	sigarray2 = sigfile.get_array()
+	assert np.array_equal(sigarray.values, sigarray2.values)
+	assert np.array_equal(sigarray.bounds, sigarray2.bounds)
+
+
+def test_iter(sigarray, sigfile):
+	"""Test iterating over signatures."""
+
+	for sig1, sig2 in zip(sigarray, sigfile.iter_signatures()):
+		assert np.array_equal(sig1, sig2)
