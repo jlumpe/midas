0a57d972c481e0b38b5a4a0e05a1c0dc621b475e
Jared Lumpe <mjlumpe@gmail.com>
2020-05-21 08:41:08 -0700

    Updates to midas.remote module

--- midas/remote.py
+++ midas/remote.py
@@ -1,18 +1,23 @@
 """Utilities for downloading remote data, e.g. from GenBank"""
 
-import sys
 import io
 import shutil
 import contextlib
 from urllib.request import urlopen
 from urllib.error import URLError
+from threading import Lock
 
-from concurrent.futures import ThreadPoolExecutor, as_completed
-import threading
+from .parallel import AbortableThreadPoolExecutor
 
 
 class URLRetryError(Exception):
-	"""Raised by urlopen_retry when all attempts fail"""
+	"""Raised by urlopen_retry when all attempts fail.
+
+	Attributes
+	----------
+	attempt_errors : list
+		List of errors encountered on all attempts.
+	"""
 
 	def __init__(self, message, attempt_errors):
 		attempt_errors = tuple(attempt_errors)
@@ -21,7 +26,26 @@ class URLRetryError(Exception):
 
 
 def urlopen_retry(url, attempts=3, **kwargs):
-	"""Calls urlopen() but retries a given number of times after failure"""
+	"""Calls ``urlopen()`` but retries a given number of times after failure.
+
+	Parameters
+	----------
+	url : str
+	attempts : int
+		Number of attempts to make.
+	kwargs :
+		Passed to :func:`urllib.request.urlopen`.
+
+	Returns
+	-------
+		Readable file-like object.
+
+	Raises
+	------
+	URLRetryError
+		If maximum number of attempts exceeded. Context is the error encountered
+		on the most recent attempt.
+	"""
 
 	errors = []
 	attempts_made = 0
@@ -40,11 +64,11 @@ def urlopen_retry(url, attempts=3, **kwargs):
 	raise URLRetryError(msg, errors) from errors[-1]
 
 
-def download_sequence(db, genome, url, db_lock=None, store_opts=dict(),
+def download_sequence(db, genome_id, url, db_lock=None, store_opts=dict(),
                       aborted=None, **kwargs):
-	"""Download sequence for genome and store it
+	"""Download sequence for genome and store it in a database.
 
-	Used as thread worker for download_genome_sequences.
+	Used as thread worker for :func:`.download_genome_sequences`.
 	"""
 
 	# Download as bytes
@@ -74,135 +98,115 @@ def download_sequence(db, genome, url, db_lock=None, store_opts=dict(),
 			exitstack.enter_context(db_lock)
 
 		if aborted is None or not aborted():
-			db.store_sequence(genome.id, buf, src_mode='b', **store_opts)
+			db.store_sequence(genome_id, buf, src_mode='b', **store_opts)
 			return True
-		else:
-			return False
+
+		return False
 
 
 def download_sequences_parallel(db, items, **kwargs):
-	"""Download and store set of sequences in parallel
+	"""Download a large set of sequences in parallel and store in a database.
 
 	This function will regularly be downloading tens of thousands of sequences
 	at a time over several hours, so it needs to be able to exit gracefully.
 	This is difficult with threads. It will attempt to abort all pending jobs
-	and wait unti completion if any errors are encountered in the main thread.
+	and wait until completion if any errors are encountered in the main thread.
 	This includes the first KeyboardInterrupt, which will print a warning
 	message, but subsequent ones will exit immediately.
+
+	Parameters
+	----------
+	db : midas.database.base.AbstractDatabase
+	items :
+		Collection of ``(genome, url)`` or ``(genome, url, store_opts)`` tuples
+		representing sequences to download.
+	max_workers : int
+		Maximum number of worker threads to use
+	store_opts : dict
+		Keyword arguments to :meth:`midas.database.base.AbstractDatabase.store_sequence`
+		to use for all items.
+	callback : callable
+		Function with signature ``(genome, exc)`` that is called (in the main
+		thread) for each item that completes. If the download failed the exception
+		which caused the failure will be passed as ``exc``, otherwise this
+		argument will be None. This function may return ``False`` to signal that
+		the worker pool should abort all future jobs and shut down.
+
+	Additional keyword arguments are passed to :func:`urlopen_retry`.
+
+	Returns
+	-------
+	bool
+		False if the process was aborted before it could finish, True otherwise.
 	"""
 
 	max_workers = kwargs.pop('max_workers', None)
 	common_store_opts = kwargs.pop('store_opts', dict())
 	callback = kwargs.pop('callback', None)
 
-	with ThreadPoolExecutor(max_workers) as executor:
+	with AbortableThreadPoolExecutor(max_workers) as executor:
 
 		# Dictionary to get genomes by their future when finished
 		genomes_by_future = dict()
 
 		# Lock around database usage
-		db_lock = threading.Lock()
+		db_lock = Lock()
 
-		# Flag to inform worker threads that abort has happened
-		is_aborted = False
-		check_aborted = lambda: is_aborted
+		# Submit the tasks
+		for item in items:
 
-		# Cancels all pending futures and sets abort flag
-		def abort():
-			nonlocal is_aborted
-			is_aborted = True
-			for future in genomes_by_future.keys():
-				future.cancel()
+			if len(item) == 2:
+				genome, url = item
+				store_opts = common_store_opts
+			elif len(item) == 3:
+				genome, url, store_opts = item
+			else:
+				raise ValueError('Download items shoud be 2- or 3-tuples')
 
-		try:
-
-			# Submit the tasks
-			for item in items:
-
-				if len(item) == 2:
-					genome, url = item
-					store_opts = common_store_opts
-				elif len(item) == 3:
-					genome, url, store_opts = item
-				else:
-					raise ValueError('Download items shoud be 2- or 3-tuples')
-
-				future = executor.submit(
-					download_sequence,
-					db,
-					genome,
-					url,
-					db_lock=db_lock,
-					store_opts=store_opts,
-					aborted=check_aborted,
-					**kwargs
-				)
-				genomes_by_future[future] = genome
-
-		# If any errors in submitting jobs
-		except BaseException as exc:
-			abort()
-			raise
+			future = executor.submit(
+				download_sequence,
+				db,
+				genome.id,
+				url,
+				db_lock=db_lock,
+				store_opts=store_opts,
+				aborted=executor.is_aborted,
+				**kwargs
+			)
+			genomes_by_future[future] = genome
 
 		# Iterator over futures as they complete
-		completed = iter(as_completed(genomes_by_future.keys()))
+		# We only actually care about the result if we need to send it to
+		# the callback, but we still want to iterate over all of them to ensure
+		# the function does not return until all futures are completed
+		# (successfully or unsuccessfully).
+		for future in executor.as_completed():
 
-		# Don't use for loop as we want to wrap every call to
-		# completed.__next__ in a try-catch
-		while True:
+			genome = genomes_by_future[future]
 
-			# Catch exceptions anywhere in loop
-			try:
+			# Get result for callback
+			if callback is not None and not future.cancelled():
 
-				# Get next completed future
+				# Getting the result will raise any exception that
+				# occured in the worker thread
 				try:
-					future = next(completed)
-				except StopIteration:
-					break
+					genome_added = future.result()
 
-				genome = genomes_by_future[future]
-
-				# Get result for callback
-				if callback is not None and not future.cancelled():
-
-					# Getting the result will raise any exception that
-					# occured in the worker thread
-					try:
-						genome_added = future.result()
-
-					# Call callback with exception
-					except Exception as exc:
-						callback_return = callback(genome, exc)
-
-					else:
-						# Worker returns True if not aborted
-						if genome_added:
-							# Worker finished, call callback
-							callback_return = callback(genome, None)
-						else:
-							# Abort has already happened, skip
-							callback_return = None
-
-					# If False returned from callback, abort
-					if callback_return is False:
-						abort()
-						break
-
-			# Cancel pending futures on first KeyboardInterrupt but keep
-			# looping
-			except KeyboardInterrupt:
-				if not is_aborted:
-					print('Keyboard interrupt caught, attempting to shut down '
-					      'worker pool gracefully...',
-					      file=sys.stderr)
-					abort()
+				# Call callback with exception
+				except Exception as exc:
+					callback_return = callback(genome, exc)
 
 				else:
-					raise
+					# Worker returns True if not aborted
+					if genome_added:
+						# Worker finished, call callback
+						callback_return = callback(genome, None)
+					else:
+						# Abort has already happened, skip
+						callback_return = None
 
-			# All other exceptions abort and then propagate
-			except Exception:
-				abort()
-				raise
+				# If False returned from callback, abort
+				if callback_return is False:
+					executor.abort()
 
-		return not is_aborted
+	return not executor.is_aborted()
