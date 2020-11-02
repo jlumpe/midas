9e23348f57b1cdf6179b1dc88d624d15e954e7ca
Jared Lumpe <mjlumpe@gmail.com>
2020-11-02 11:50:47 -0700

    midas.db.migrate.init_db() function

--- midas/db/migrate/__init__.py
+++ midas/db/migrate/__init__.py
@@ -5,6 +5,8 @@ This package also contains all Alembic data files.
 
 from alembic.config import Config
 from alembic import command
+from alembic.migration import MigrationContext
+from alembic.script import ScriptDirectory
 from pkg_resources import resource_filename
 
 
@@ -55,3 +57,42 @@ def upgrade(engine, revision='head', tag=None, **kwargs):
 	"""
 	config = get_alembic_config(engine, **kwargs)
 	command.upgrade(config, revision, tag=tag)
+
+
+def init_db(engine):
+	"""Initialize the database schema by creating all tables and stamping with the latest Alembic revision.
+
+	Expects a fresh database that does not already contain any  tables for the :mod:`midas.db.models`
+	models and has not had any migrations run on it yet.
+
+	Parameters
+	----------
+	engine : sqlalchemy.engine.base.Engine
+		SQLAlchemy engine specifying database connection info.
+
+	Raises
+	------
+	RuntimeError
+		If the database is already stamped with an Alembic revision.
+	sqlalchemy.exc.OperationalError
+		If any of the database tables to be created already exist.
+	"""
+	from midas.db.models import Base
+
+	conf = get_alembic_config()
+	script = ScriptDirectory.from_config(conf)
+
+	with engine.connect() as conn:
+		ctx = MigrationContext.configure(conn)
+
+		# Check there is no current revision stamped
+		current = ctx.get_current_revision()
+		if current is not None:
+			raise RuntimeError('Expected uninitialized database, but current alembic revision is %s' % current)
+
+		# Create tables
+		# Set checkfirst=false so that we get an SQL error if any tables already exist
+		Base.metadata.create_all(conn, checkfirst=False)
+
+		# Stamp latest alembic version
+		ctx.stamp(script, 'head')
