aaa8938851ab853bb02d90b66f2069afb235b2c7
Jared Lumpe <mjlumpe@gmail.com>
2021-06-11 19:40:10 -0600

    Move make_empty_db and testdb_* fixtures to root conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,10 @@ from pathlib import Path
 
 import numpy as np
 import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from midas.db.models import Base as models_base
 
 
 @pytest.fixture(scope='session')
@@ -19,3 +23,27 @@ def raise_numpy_errors():
 	yield
 
 	np.seterr(**old_settings)  # Not really necessary
+
+
+@pytest.fixture(scope='session')
+def make_empty_db():
+	"""Function which creates an empty in-memory-database with initialized schema."""
+	def empty_db_factory():
+		engine = create_engine('sqlite:///:memory:')
+		models_base.metadata.create_all(engine)
+		return engine
+
+	return empty_db_factory
+
+
+@pytest.fixture(scope='session')
+def testdb_dir(test_data):
+	"""Directory containing testdb_210126 data."""
+	return test_data / 'testdb_210126'
+
+
+@pytest.fixture(scope='session')
+def testdb_session(testdb_dir):
+	"""Function which creates a new session for the test database."""
+	engine = create_engine('sqlite:///' + str(testdb_dir / 'testdb_210126.db'))
+	return sessionmaker(engine)
