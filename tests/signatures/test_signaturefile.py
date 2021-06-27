8a7c7b36fd239553e9059f839a13f0764c320eef
Jared Lumpe <mjlumpe@gmail.com>
2021-06-27 14:07:22 -0600

    Group SignatureFile tests in class

--- tests/signatures/test_signaturefile.py
+++ tests/signatures/test_signaturefile.py
@@ -220,148 +220,140 @@ class ProgressChecker:
 		assert self.last_value == self.total
 
 
-@pytest.fixture(scope='module')
-def sigarray():
-	"""Array of random signatures."""
+class TestSignatureFile:
 
-	random = np.random.RandomState(0)
+	@pytest.fixture(scope='class')
+	def sigarray(self):
+		"""Array of random signatures."""
 
-	lengths = random.randint(2000, 10000, size=20)
+		random = np.random.RandomState(0)
 
-	return SignatureArray([
-		np.sort(random.choice(4 ** 11, replace=False, size=len_))
-		for len_ in lengths
-	])
+		lengths = random.randint(2000, 10000, size=20)
 
+		return SignatureArray([
+			np.sort(random.choice(4 ** 11, replace=False, size=len_))
+			for len_ in lengths
+		])
 
-@pytest.fixture(params=[None, 'strings', 'ints'])
-def ids(sigarray, request):
-	"""None or list of strings or ints."""
-	if request.param == 'strings':
-		return ['id' + str(i) for i in range(len(sigarray))]
+	@pytest.fixture(params=[None, 'strings', 'ints'])
+	def ids(self, sigarray, request):
+		"""None or list of strings or ints."""
+		if request.param == 'strings':
+			return ['id' + str(i) for i in range(len(sigarray))]
 
-	elif request.param == 'ints':
-		return list(range(len(sigarray)))
-
-
-@pytest.fixture(params=[False, True])
-def metadata(request):
-	"""None or a dict of fake metadata."""
-	if request.param:
-		return dict(foo=1, bar='z', baz=[5, 4, 1], bleh=dict(a=1, b=None))
-	else:
-		return None
+		elif request.param == 'ints':
+			return list(range(len(sigarray)))
 
 
-@pytest.fixture
-def signature_buffer(sigarray, ids, metadata):
-	"""Binary buffer with signature file written to it."""
+	@pytest.fixture(params=[False, True])
+	def metadata(self, request):
+		"""None or a dict of fake metadata."""
+		if request.param:
+			return dict(foo=1, bar='z', baz=[5, 4, 1], bleh=dict(a=1, b=None))
+		else:
+			return None
 
-	buf = io.BytesIO()
-	SignatureFile.write(buf, sigarray, ids=ids, metadata=metadata)
-	buf.seek(0)
+	@pytest.fixture
+	def signature_buffer(self, sigarray, ids, metadata):
+		"""Binary buffer with signature file written to it."""
 
-	return buf
+		buf = io.BytesIO()
+		SignatureFile.write(buf, sigarray, ids=ids, metadata=metadata)
+		buf.seek(0)
 
+		return buf
 
-@pytest.fixture
-def sigfile(signature_buffer):
-	"""SignatureFile read from buffer."""
-	return SignatureFile(signature_buffer)
+	@pytest.fixture
+	def sigfile(self, signature_buffer):
+		"""SignatureFile read from buffer."""
+		return SignatureFile(signature_buffer)
 
+	def test_attrs(self, sigarray, sigfile):
+		"""Test basic attributes set when file is first opened."""
 
-def test_attrs(sigarray, sigfile):
-	"""Test basic attributes set when file is first opened."""
+		assert sigfile.count == len(sigarray)
+		assert np.array_equal(sigfile.lengths, list(map(len, sigarray)))
+		assert sigfile.nelems == sum(map(len, sigarray))
+		assert sigfile.dtype == sigarray.values.dtype
 
-	assert sigfile.count == len(sigarray)
-	assert np.array_equal(sigfile.lengths, list(map(len, sigarray)))
-	assert sigfile.nelems == sum(map(len, sigarray))
-	assert sigfile.dtype == sigarray.values.dtype
+	def test_ids(self, sigarray, sigfile, ids):
+		"""Test the ids attribute matches what was written."""
 
+		if ids is None:
+			assert sigfile.ids is None
 
-def test_ids(sigarray, sigfile, ids):
-	"""Test the ids attribute matches what was written."""
+		else:
+			assert np.array_equal(ids, sigfile.ids)
 
-	if ids is None:
-		assert sigfile.ids is None
+	def test_metadata(self, sigarray, sigfile, metadata):
+		"""Test the file metadata matches what was written."""
 
-	else:
-		assert np.array_equal(ids, sigfile.ids)
+		if metadata is None:
+			assert not sigfile.has_metadata
+			assert sigfile.get_metadata() is None
+		else:
+			assert sigfile.has_metadata
+			assert sigfile.get_metadata() == metadata
 
+	@pytest.mark.parametrize('chunksize', [None, 1, 3, 100])
+	@pytest.mark.parametrize('progress', [False, True])
+	def test_read_all(self, sigarray, sigfile, chunksize, progress):
+		"""Test reading all signatures into an array."""
 
-def test_metadata(sigarray, sigfile, metadata):
-	"""Test the file metadata matches what was written."""
+		# Progress callback
+		if progress:
+			callback = ProgressChecker(
+				len(sigarray),
+				len(sigarray) if chunksize is None else chunksize
+			)
+		else:
+			callback = None
 
-	if metadata is None:
-		assert not sigfile.has_metadata
-		assert sigfile.get_metadata() is None
-	else:
-		assert sigfile.has_metadata
-		assert sigfile.get_metadata() == metadata
+		# Read signatures
+		sigarray2 = sigfile.get_array(chunksize=chunksize, progress=callback)
 
+		assert np.array_equal(sigarray.values, sigarray2.values)
+		assert np.array_equal(sigarray.bounds, sigarray2.bounds)
 
-@pytest.mark.parametrize('chunksize', [None, 1, 3, 100])
-@pytest.mark.parametrize('progress', [False, True])
-def test_read_all(sigarray, sigfile, chunksize, progress):
-	"""Test reading all signatures into an array."""
+		# Check progress callback was called the correct number of times
+		if progress:
+			callback.check_called()
 
-	# Progress callback
-	if progress:
-		callback = ProgressChecker(
-			len(sigarray),
-			len(sigarray) if chunksize is None else chunksize
-		)
-	else:
-		callback = None
+	@pytest.mark.parametrize('progress', [False, True])
+	def test_read_subset(self, sigarray, sigfile, progress):
+		"""Test reading a subset of signatures into an array."""
 
-	# Read signatures
-	sigarray2 = sigfile.get_array(chunksize=chunksize, progress=callback)
+		# Random set of indices
+		n = len(sigarray)
+		random = np.random.RandomState()
+		indices = random.choice(n, n // 2)
 
-	assert np.array_equal(sigarray.values, sigarray2.values)
-	assert np.array_equal(sigarray.bounds, sigarray2.bounds)
+		# Progress callback
+		callback = ProgressChecker(len(indices)) if progress else None
 
-	# Check progress callback was called the correct number of times
-	if progress:
-		callback.check_called()
+		# Read subset of signatures
+		subarray = sigfile.get_array(indices, progress=callback)
 
+		assert len(subarray) == len(indices)
 
-@pytest.mark.parametrize('progress', [False, True])
-def test_read_subset(sigarray, sigfile, progress):
-	"""Test reading a subset of signatures into an array."""
+		for idx, sig in zip(indices, subarray):
+			assert np.array_equal(sig, sigarray[idx])
 
-	# Random set of indices
-	n = len(sigarray)
-	random = np.random.RandomState()
-	indices = random.choice(n, n // 2)
+		# Check progress callback was called the correct number of times
+		if progress:
+			callback.check_called()
 
-	# Progress callback
-	callback = ProgressChecker(len(indices)) if progress else None
+	def test_iter(self, sigarray, sigfile):
+		"""Test iterating over signatures."""
 
-	# Read subset of signatures
-	subarray = sigfile.get_array(indices, progress=callback)
+		for sig1, sig2 in zip(sigarray, sigfile.iter_signatures()):
+			assert np.array_equal(sig1, sig2)
 
-	assert len(subarray) == len(indices)
+	def test_context(self, sigfile):
+		"""Test context manager methods."""
 
-	for idx, sig in zip(indices, subarray):
-		assert np.array_equal(sig, sigarray[idx])
+		with sigfile as obj:
+			assert obj is sigfile
+			assert not sigfile.fobj.closed
 
-	# Check progress callback was called the correct number of times
-	if progress:
-		callback.check_called()
-
-
-def test_iter(sigarray, sigfile):
-	"""Test iterating over signatures."""
-
-	for sig1, sig2 in zip(sigarray, sigfile.iter_signatures()):
-		assert np.array_equal(sig1, sig2)
-
-
-def test_context(sigfile):
-	"""Test context manager methods."""
-
-	with sigfile as obj:
-		assert obj is sigfile
-		assert not sigfile.fobj.closed
-
-	assert sigfile.fobj.closed
+		assert sigfile.fobj.closed
