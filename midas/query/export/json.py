7e05ffb3e5728391054d14c3d0bd31b11250515b
Jared Lumpe <mjlumpe@gmail.com>
2021-07-03 21:44:50 -0600

    Update JSON exporter for Python 3.7

--- midas/query/export/json.py
+++ midas/query/export/json.py
@@ -1,7 +1,7 @@
 """Export results to JSON."""
 
 import json
-from functools import singledispatchmethod
+import sys
 
 from attr import attrs, attrib, asdict
 
@@ -9,6 +9,25 @@ from .base import AbstractResultsExporter
 from midas.query.results import QueryResults, QueryResultItem
 from midas.db.models import ReferenceGenomeSet, Taxon
 import midas.io.json as mjson
+
+
+if sys.version_info[1] >= 8:
+	from functools import singledispatchmethod
+
+else:
+	# Not available in 3.7, make simple implementation
+	from functools import singledispatch, wraps
+
+	def singledispatchmethod(func):
+		dispatcher = singledispatch(func)
+
+		@wraps(func)
+		def wrapper(self, arg, *rest, **kw):
+			impl = dispatcher.dispatch(type(arg))
+			return impl(self, arg, *rest, **kw)
+
+		wrapper.register = dispatcher.register
+		return wrapper
 
 
 @attrs()
@@ -32,19 +51,19 @@ class JSONResultsExporter(AbstractResultsExporter):
 		# Base case
 		return mjson.to_json(obj)
 
-	@_to_json.register
+	@_to_json.register(QueryResults)
 	def _results_to_json(self, results: QueryResults):
 		return asdict(results)
 
-	@_to_json.register
+	@_to_json.register(QueryResultItem)
 	def _item_to_json(self, item: QueryResultItem):
 		return asdict(item)
 
-	@_to_json.register
+	@_to_json.register(ReferenceGenomeSet)
 	def _genomeset_to_json(self, gset: ReferenceGenomeSet):
 		return self._todict(gset, ['id', 'key', 'version', 'name', 'description'])
 
-	@_to_json.register
+	@_to_json.register(Taxon)
 	def _taxon_to_json(self, taxon: Taxon):
 		return self._todict(taxon, ['id', 'name', 'ncbi_id', 'distance_threshold'])
 
