49d3eb2108e810d60985789ea3cd3349eb43a1ba
Jared Lumpe <mjlumpe@gmail.com>
2021-07-05 22:05:18 -0600

    Edit fresh alembic env.py to work with migrate.get_alembic_config()

--- midas/db/migrate/alembic/env.py
+++ midas/db/migrate/alembic/env.py
@@ -1,8 +1,5 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -15,9 +12,8 @@ fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from midas.db.models import Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -28,18 +24,11 @@ target_metadata = None
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
 
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
+    Since we don't have a connection URL written into alembic.ini, we need to specify the
+    "dialect_name" argument.
     """
-    url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url,
+        dialect_name='sqlite',
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -52,15 +41,11 @@ def run_migrations_offline():
 def run_migrations_online():
     """Run migrations in 'online' mode.
 
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
+    Expects a value for the "connectable" argument to migrate.get_alembic_config().
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = config.attributes.get('connectable')
+    if connectable is None:
+        raise RuntimeError('Connectable object must be passed to midas.db.migrate.get_alembic_config()')
 
     with connectable.connect() as connection:
         context.configure(
